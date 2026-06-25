1a59f8572c59d4a1197b46bc0c88abeca38799ca
Yash Meshiya <meshiyayash@gmail.com>
2026-06-25 17:46:50 +0000

    add 0098-validate_binary_tree.cpp

---
+++ week4_mandatory/0098-validate_binary_tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool check(TreeNode* root, long long l, long long h) {
+        if (root == NULL) return true;
+
+        return (root->val > l && root->val < h) &&
+               check(root->left, l, root->val) &&
+               check(root->right, root->val, h);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return check(root, LLONG_MIN, LLONG_MAX);
+    }
+};

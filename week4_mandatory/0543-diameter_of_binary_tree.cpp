e092511e6bd5b7fa0f3dc92e5903818642720ef7
Yash Meshiya <meshiyayash@gmail.com>
2026-06-27 09:17:54 +0000

    refactor: resolve 0543-diameter_of_binary_tree.cpp

--- week4_mandatory/0543-diameter_of_binary_tree.cpp
+++ week4_mandatory/0543-diameter_of_binary_tree.cpp
@@ -1,23 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-    int res = 0;
-
 public:
-    int diameterOfBinaryTree(TreeNode* root) {
-        dfs(root);
-        return res;        
-    }
-
-private:
-    int dfs(TreeNode* root) {
-        if (!root) {
+    int depth(TreeNode*root,int&ans){
+        if(root==NULL){
             return 0;
         }
+        int a=depth(root->left,ans);
+        int b=depth(root->right,ans);
+        ans=max(ans,a+b);
+        return max(a,b)+1;
+    }
 
-        int l = dfs(root->left);
-        int r = dfs(root->right);
-
-        res = std::max(res, l + r);
-
-        return 1 + std::max(l, r);
-    }    
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans=0;
+        int x=depth(root,ans);
+        return ans;
+    }
 };

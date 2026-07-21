c771b2edc0806b48eb1a3fff4ed16e590a3fd5c4
Yash Meshiya <meshiyayash@gmail.com>
2026-07-21 21:28:40 +0000

    feat: implemented bestOrder function

--- maze_solver/maze_solver.cpp
+++ maze_solver/maze_solver.cpp
@@ -1,7 +1,4 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include <queue>
+#include<bits/stdc++.h>
 using namespace std;
 
 
@@ -67,14 +64,59 @@ Result bfs      (const Grid& g, pair<int,int> s, pair<int,int> t){
 }
 
 
-Result dfsPath  (const Grid& g, pair<int,int> s, pair<int,int> t);
+// Result dfsPath  (const Grid& g, pair<int,int> s, pair<int,int> t);
 Result dijkstra (const Grid& g, pair<int,int> s, pair<int,int> t);
 Result astar    (const Grid& g, pair<int,int> s, pair<int,int> t);
 
+void shortest(const vector<vector<int>>& dist,vector<int>&indx,int&cur_cost,int &best,vector<bool>&vis,vector<int>& outOrder){
+// base case ---> if i have visited all the index from start then I should go to the goal
+    if(indx.size() == vis.size()){
+        cur_cost+=dist[indx.back()][dist.size()-1];
+        indx.push_back(dist.size()-1);
+        if(cur_cost<best){
+            best=cur_cost;
+            outOrder=indx;
+        }
+        indx.pop_back();
+        cur_cost-=dist[indx.back()][dist.size()-1];
+        return;
+    }
+
+// Pruning step ---> IF at any moment my current distance is greater than best I should avoid that path
+    if(cur_cost>=best){
+        return;
+    }
+// Recursively visiting each coin and finding minimum cost/distance
+    for(int i=1;i+1<=vis.size();i++){
+        if(vis[i]){
+            continue;
+        }
+
+        cur_cost+=dist[indx.back()][i];
+        vis[i]=true;
+        indx.push_back(i);
+        shortest(dist,indx,cur_cost,best,vis,outOrder);
+
+        //backtracing
+        indx.pop_back();
+        cur_cost-=dist[indx.back()][i];
+        vis[i]=false;
+    }
+}
+
 // Coin-order backtracking:
-int bestOrder(const vector<vector<int>>& dist,
-              int startIdx, int goalIdx,
-              vector<int>& outOrder);
+int bestOrder(const vector<vector<int>>& dist,int startIdx, int goalIdx, vector<int>& outOrder){
+    int best=INT_MAX ,cur_cost=0;
+
+    vector<bool> vis(dist.size()-1,false);
+    vis[0]=true;
+
+    vector<int> indx;
+    indx.push_back(0);
+    shortest(dist,indx,cur_cost,best,vis,outOrder);
+    
+    return best;
+}
 
 
 
@@ -98,6 +140,7 @@ int main(){
 
 // Storing the key points
     vector<pair<int,int >> keypoints;
+    int total_keypoints;
     keypoints.push_back(start);
 
     for(int i=0;i<g.R;i++){
@@ -109,18 +152,22 @@ int main(){
     }
 
     keypoints.push_back(goal);
-
+    total_keypoints=keypoints.size();
 // K+2 * K+2 matrix for storing min distance between each key point
-    vector<vector<int>> key_points_distance(keypoints.size(),vector<int>(keypoints.size(),0));
+    vector<vector<int>> key_points_distance(total_keypoints,vector<int>(total_keypoints,0));
 
-    for(int i=0;i<keypoints.size();i++){
-        for(int j=i;j<keypoints.size();j++){
+    for(int i=0;i<total_keypoints;i++){
+        for(int j=i;j<total_keypoints;j++){
             key_points_distance[i][j]=bfs(g,keypoints[i],keypoints[j]).cost;
             key_points_distance[j][i]=key_points_distance[i][j];
         }
     }
 
+// Finding best path for non weighted graphs
+    vector <int> non_weighted_path;
+    int non_weighted_path_length=bestOrder(key_points_distance,0,total_keypoints-1,non_weighted_path);
 
+    cout << non_weighted_path_length << endl;
     return 0;
 }
 
